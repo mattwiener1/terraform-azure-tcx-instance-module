4b0f8f93d7e4ad70557488b7ca98ebc6d8747a1a
Ahmad Obay <ahmad.obay@gmail.com>
2019-06-11 18:45:31 +0200

    Changing the Subnet Name

--- AzureSubnet.tf
+++ AzureSubnet.tf
@@ -1,5 +1,5 @@
 resource "azurerm_subnet" "AzureSubnet" {
-  name                 = "internal"
+  name                 = "${azurerm_resource_group.AzureResourceGroup.name}-Subnet"
   resource_group_name  = "${azurerm_resource_group.AzureResourceGroup.name}"
   virtual_network_name = "${azurerm_virtual_network.AzureVirtualNetwork.name}"
   address_prefix       = "10.100.150.0/24"
