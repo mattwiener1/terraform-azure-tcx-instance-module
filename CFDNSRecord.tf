536f7f79c79a4f148b94c13f223febabe67c536d
Ahmad Obay <ahmad.obay@gmail.com>
2019-07-01 10:37:44 +0200

    Correcting value in CF record

--- CFDNSRecord.tf
+++ CFDNSRecord.tf
@@ -1,7 +1,7 @@
 resource "cloudflare_record" "CFDNSRecord" {
   domain   = "${lower(var.CustomerSubdomain)}.${var.VendorDomainName}"
   name     = "@"
-  value    = ["${azurerm_public_ip.AzurePublicIP.ip_address}"]
+  value    = "${azurerm_public_ip.AzurePublicIP.ip_address}"
   type     = "A"
   ttl      = 3600
   priority = 0
