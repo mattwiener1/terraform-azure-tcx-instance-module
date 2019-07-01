cb32be08c42325c4773c191377f3c4b01f85501b
Ahmad Obay <ahmad.obay@gmail.com>
2019-07-01 10:14:40 +0200

    Replacing Route53 with Cloudflare

---
+++ CFDNSRecord.tf
@@ -0,0 +1,8 @@
+resource "cloudflare_record" "CFDNSRecord" {
+  domain   = "${lower(var.CustomerSubdomain)}.${var.VendorDomainName}"
+  name     = "@"
+  value    = ["${azurerm_public_ip.AzurePublicIP.ip_address}"]
+  type     = "A"
+  ttl      = 3600
+  priority = 0
+}
