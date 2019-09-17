eade6aaf0157b067c12240b10ddd3e03514ff363
Ahmad Obay <obay@companytone.com>
2019-09-17 09:15:57 -0400

    forcing security group to depend on resource group

--- AzureSecurityGroup.tf
+++ AzureSecurityGroup.tf
@@ -2,5 +2,6 @@ resource "azurerm_network_security_group" "AzureSecurityGroup" {
   name                = "${azurerm_resource_group.AzureResourceGroup.name}-SecurityGroup"
   location            = azurerm_resource_group.AzureResourceGroup.location
   resource_group_name = azurerm_resource_group.AzureResourceGroup.name
+  depends_on          = ["azurerm_resource_group.AzureResourceGroup"]
 }
 
